eb9dd554a5402340eb2ebc083d55e83506fd7204
vivek raj <raushanitpkumar@gmail.com>
2025-02-12 02:23:58 +0530

    new

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Job Portal deployed new version </h1>");
 });
+
+app.get("/home", (req, res) => {
+  res.send({
+    name: "vivek raj",
+  });
+});
 const port = process.env.PORT;
 app.listen(8080, () => {
   console.log(`Node server running on ${port}`);
